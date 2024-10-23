7bf8db8a4f8abc5d1d5d6726f5d3a674b4192853
Kris1027 <obarzanek.work@gmail.com>
2024-10-23 18:56:23 +0200

    add css reset

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-        <h1>e-commerce</h1>
+        <h1>E-commerce</h1>
         <p>loading...</p>
     </div>
   );

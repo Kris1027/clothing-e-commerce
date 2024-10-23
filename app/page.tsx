f0756a1216eace096e4cd9d9d47cda741562d080
Kris1027 <obarzanek.work@gmail.com>
2024-10-23 18:36:01 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <h1>e-commerce</h1>
+        <p>loading...</p>
+    </div>
+  );
+}

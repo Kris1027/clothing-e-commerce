73c83398d126cbb3e3134960746b4ff9926e2992
Kris1027 <obarzanek.work@gmail.com>
2024-10-24 12:50:45 +0200

    build divider and implement in layout

---
+++ app/_components/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import styles from '@/app/_components/Divider/Divider.module.css';
+import { FC } from 'react';
+
+const Divider: FC = () => {
+   return <div className={styles.divider}></div>;
+};
+
+export default Divider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,32 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
-import {Montserrat} from 'next/font/google'
+import { Montserrat } from 'next/font/google';
 import TopBar from '@/app/_components/TopBar/TopBar';
+import Divider from './_components/Divider/Divider';
 
 const montserrat = Montserrat({
-    weight: ['400', '500', '700'],
-    subsets: ['latin'],
-})
+   weight: ['400', '500', '700'],
+   subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-    title: 'Clothing E-Commerce',
-    description: 'Premium clothing store offering curated collections for men and women. Discover trendy casual wear, business attire, and designer accessories.',
+   title: 'Clothing E-Commerce',
+   description:
+      'Premium clothing store offering curated collections for men and women. Discover trendy casual wear, business attire, and designer accessories.',
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+   children,
+}: Readonly<{
+   children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-        <body className={montserrat.className}>
-        <TopBar/>
-        {children}
-        </body>
-        </html>
-    );
+   return (
+      <html lang='pl'>
+         <body className={montserrat.className}>
+            <TopBar />
+            <Divider />
+            {children}
+         </body>
+      </html>
+   );
 }
